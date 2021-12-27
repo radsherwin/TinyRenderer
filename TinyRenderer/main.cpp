604e97e37a5e5eb56d9ef6b902dafb774589840e
radsherwin <radsherwin@gmail.com>
2021-12-27 12:41:57 -0600

    Initial Commit

---
+++ TinyRenderer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TinyRenderer
+//
+//  Created by Sherwin Rad on 12/27/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
