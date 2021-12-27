32764c7ed7e338a28173c9e971f09c230a45c089
radsherwin <radsherwin@gmail.com>
2021-12-27 13:55:54 -0600

    output .obj as wireframe .tga

--- TinyRenderer/main.cpp
+++ TinyRenderer/main.cpp
@@ -6,9 +6,13 @@
 //
 
 #include "tgaimage.h"
+#include "model.h"
 
 const TGAColor white = TGAColor(255,255,255,255);
 const TGAColor red = TGAColor(255,0,0,255);
+Model *model = nullptr;
+const int width = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
@@ -61,14 +65,35 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    TGAImage image(100,100, TGAImage::RGB);
-    for (int i=0; i<1000000; i++) {
-        line(13, 20, 80, 40, image, white);
-        line(20, 13, 40, 80, image, red);
-        line(80, 40, 13, 20, image, red);
+    if (argc==2)
+    {
+        model = new Model(argv[1]);
     }
+    else
+    {
+        model = new Model("/Users/radsherwin/Documents/Xcode/TinyRenderer/TinyRenderer/Models/african_head.obj");
+    }
+    
+    TGAImage image(width, height, TGAImage::RGB);
+    for(int i = 0; i < model->nFaces(); i++)
+    {
+        std::vector<int> face = model->face(i);
+        for(int j = 0; j < 3; j++)
+        {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j+1)%3]);
+            int x0 = (v0.x + 1) * width/2;
+            int y0 = (v0.y + 1) * height/2;
+            int x1 = (v1.x + 1) * width/2;
+            int y1 = (v1.y + 1) * height/2;
+            line(x0, y0, x1, y1, image, white);
+            
+        }
+    }
+    
     image.flip_vertically();
     image.write_tga_file("output.tga");
+    delete model;
     return 0;
+    
 }
---
+++ TinyRenderer/model.h
@@ -0,0 +1,34 @@
+//
+//  model.h
+//  TinyRenderer
+//
+//  Created by Sherwin Rad on 12/27/21.
+//
+
+#ifndef model_h
+#define model_h
+
+#include <vector>
+#include "geometry.h"
+
+class Model
+{
+private:
+    std::vector<Vec3f> verts_;
+    std::vector<std::vector<int>> faces_;
+public:
+    Model(const char* const fileName);
+    Model(const Model&) =delete;
+    Model& operator=(const Model&) = delete;
+    Model(Model&&) = delete;
+    Model& operator=(Model&&) = delete;
+    ~Model();
+    
+    int nVerts();
+    int nFaces();
+    Vec3f vert(int i);
+    std::vector<int> face(int idx);
+};
+
+
+#endif /* model_h */
